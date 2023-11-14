716288c36b280755344ae03f2615fd1dc3858530
道乐 丁 <604929522@qq.com>
2023-11-14 19:27:32 +0800

    11.14 部分函数添加

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,8 @@
   <!-- <selectedPlane></selectedPlane> //处理平面-->
   <!-- <DrawModel></DrawModel> //绘制模型-->
   <!-- <modelCzml></modelCzml> //卫星模型 -->
-  <CallbackProperty></CallbackProperty>
+  <!-- <CallbackProperty></CallbackProperty> //回调函数-->
+  <ClampTo3DModel></ClampTo3DModel>
 </template>
 
 <script setup>
@@ -13,7 +14,8 @@ import MOdel_Coloring from "../src/Test/2.3D_Model_Coloring.vue";
 import selectedPlane from "../src/Test/3.裁剪平面.vue"
 import DrawModel from "../src/Test/4.绘制模型.vue"
 import modelCzml from "../src/Test/5.卫星轨迹.vue"
-import CallbackProperty from "../src/Test/5.卫星轨迹.vue"
+import CallbackProperty from "../src/Test/6.回调函数.vue"
+import ClampTo3DModel from "../src/Test/7.添加标签.vue"
 const components = [
   chooseFeature,
   MOdel_Coloring,
@@ -21,6 +23,7 @@ const components = [
   DrawModel,
   modelCzml,
   CallbackProperty,
+  ClampTo3DModel,
 ]
 </script>
 <style scoped></style>
---
+++ src/Test/7.添加标签.vue
@@ -0,0 +1,125 @@
+<template>
+  <div id="cesiumContainer" class="fullSize"></div>
+    <div id="loadingOverlay"><h1>Loading...</h1></div>
+    <div id="toolbar"></div>
+</template>
+  
+<script setup>
+import * as Cesium from 'cesium';
+import { onMounted } from 'vue';
+import '../assets/Apps/Sandcastle/Sandcastle-header'
+onMounted(() => {
+  createViewer1()//3D Model Coloring沙盒
+})
+
+async function createViewer1() {
+  const viewer = new Cesium.Viewer("cesiumContainer", {
+  infoBox: false,
+  selectionIndicator: false,
+  shadows: true,
+  shouldAnimate: true,
+});
+const scene = viewer.scene;
+scene.globe.depthTestAgainstTerrain = true;
+
+if (!scene.sampleHeightSupported) {
+  window.alert("This browser does not support sampleHeight.");
+}
+
+const longitude = -2.1480545852753163;
+const latitude = 0.7688240036937101;
+const range = 0.000001;
+const duration = 4.0;
+
+const entity = viewer.entities.add({
+  position: Cesium.Cartesian3.fromRadians(longitude, latitude),
+  model: {
+    uri: "http://192.168.10.11:8888/cesium/gltf/GroundVehicle.glb",
+  },
+});
+
+const point = viewer.entities.add({
+  position: new Cesium.CallbackProperty(updatePosition, false),
+  point: {
+    pixelSize: 10,
+    color: Cesium.Color.YELLOW,
+
+    //获取或设置与相机的距离，在深度处禁用深度测试，例如，以防止剪切地形。设置为零时，将始终应用深度测试。
+    //设置为Number.POSITIVE_INFINITY时，永远不会应用深度测试。
+    disableDepthTestDistance: Number.POSITIVE_INFINITY,
+  },
+  label: {
+    show: false,
+    showBackground: true,
+    font: "14px monospace",
+    
+    //获取或设置此标签的水平原点，确定标签是否绘制在其锚定位置的左侧，中心或右侧。
+    horizontalOrigin: Cesium.HorizontalOrigin.LEFT,
+
+//获取或设置此标签的垂直原点，以确定标签是否为到其锚定位置的上方，下方或中心。
+    verticalOrigin: Cesium.VerticalOrigin.BOTTOM,
+
+    //获取或设置屏幕空间中距此标签原点的像素偏移量。
+    //这是常用的在同一位置对齐多个标签和广告牌，例如图像和文本。的屏幕空间原点是画布的左上角；
+    //x 从从左到右， y 从上到下增加。
+    pixelOffset: new Cesium.Cartesian2(5, 5),
+    disableDepthTestDistance: Number.POSITIVE_INFINITY,
+  },
+});
+
+const objectsToExclude = [point];
+const cartographic = new Cesium.Cartographic();
+
+function updatePosition(time, result) {
+  
+  //secondsOfDay 获取或设置当前日期的秒数。
+  const offset = (time.secondsOfDay % duration) / duration;
+
+  cartographic.longitude = longitude - range + offset * range * 2.0;
+  cartographic.latitude = latitude;
+
+  let height;
+  if (scene.sampleHeightSupported) {
+    height = scene.sampleHeight(cartographic, objectsToExclude);
+    
+  }
+
+  if (Cesium.defined(height)) {
+    cartographic.height = height;
+    point.label.text = `${Math.abs(height).toFixed(2).toString()} m`;
+    point.label.show = true;
+    
+  } else {
+    cartographic.height = 0.0;
+    point.label.show = false;
+  }
+
+  return Cesium.Cartographic.toCartesian(
+    cartographic,
+    Cesium.Ellipsoid.WGS84,
+    result
+  );
+}
+viewer.trackedEntity = entity;
+}
+
+</script>
+  
+<style scoped>
+@import url(../assets/Apps/Sandcastle/templates/bucketRaw.css);
+
+#toolbar {
+  background: rgba(42, 42, 42, 0.8);
+  padding: 4px;
+  border-radius: 4px;
+}
+
+#toolbar input {
+  vertical-align: middle;
+  padding-top: 2px;
+  padding-bottom: 2px;
+}
+
+#toolbar .header {
+  font-weight: bold;
+}</style>
